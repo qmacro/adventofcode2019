d84ae277ac7d869fc1307524b69441089da05921
DJ Adams <dj.adams@pobox.com>
2019-12-02 07:31:50 +0000

    add day starter 00.js

---
+++ day/00.js
@@ -0,0 +1,7 @@
+const { } = require('ramda')
+const input = require('./lib/utils').parseFile('00')
+
+module.exports = {
+  a: _ => 'a',
+  b: _ => 'b'
+}
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const day = (new Date()).getDate()
+let day = (new Date()).getDate()
 const file = day => `./day/${String(day).padStart(2, '0')}`
 
 const { a, b } = require(file(day))

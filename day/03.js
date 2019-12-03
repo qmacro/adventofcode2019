2ece5a6f014cb8b9e98eba3330ecd18f3d6daa9e
DJ Adams <dj.adams@pobox.com>
2019-12-03 16:39:14 +0000

    draft solution for 3a

--- day/03.js
+++ day/03.js
@@ -1,11 +1,13 @@
-const { reduce, inc, dec, last, range, concat, equals, juxt, compose, head, tail, map, split } = require('ramda')
+const { curry, apply, add, minBy, divide, __, lift, pluck, max, intersection, reduce, inc, dec, last, range, concat, equals, juxt, compose, head, tail, map, split } = require('ramda')
 const input = require('./lib/utils').parseFile('03')
 
 const DIRECTION = 0, DISTANCE = 1
 
+// Parses a single segment into direction and distance
 // R75 -> ['R', 75]
 const parseSegment = juxt([head, compose(Number, tail)])
 
+// Parses an entire path trace into a set of parsed segments
 // 'R75,D30,U83,...' -> [['R', 75], ['D', 30], ['U', 83], ...]
 const parseTrace = compose(map(parseSegment), split(/,/))
 
@@ -18,7 +20,7 @@ const dir = {
   'D': ([x, y]) => ([x, dec(y)])
 }
 
-// Returns coords for a single segment
+// Calculates all the coords for a single segment (incl origin)
 // ([0,0], ['R', 2]) -> [[0,0], [1,0], [2,0]]
 const segmentCoords = (start, segment) => reduce(
   (a, x) => concat(a, [dir[segment[DIRECTION]](last(a))]),
@@ -34,24 +36,28 @@ const pathCoords = (start, path) => reduce(
   path
 ) 
 
-console.log(pathCoords([0,0], [['R',2],['U',3]]))
+// Calculates the sum of absolute values of x and y in a coord
+// [1,2] -> 3, [4,-5] -> 9
+const sumAbsCoords = compose(apply(add), lift(Math.abs))
 
-a = _ => 'a'
-b = _ => 'b'
+const nearestCrossoverToZero = ([trace1, trace2]) => {
+  const
+    path1 = pathCoords([0,0], parseTrace(trace1)),
+    path2 = pathCoords([0,0], parseTrace(trace2))
+    crossovers = intersection(path1, path2)
+    nearest = reduce(minBy(sumAbsCoords), [Infinity,Infinity], tail(crossovers))
+    return sumAbsCoords(nearest)
+}
+
+const a = _ => nearestCrossoverToZero(input)
+const b = _ => 'b'
 
 /**
  * TESTS
  */
-console.log(
-  equals(a([
-    parseTrace('R8,U5,L5,D3'),
-    parseTrace('U7,R6,D4,L4')
-  ]), 6),
-  equals(a([
-    parseTrace('R75,D30,R83,U83,L12,D49,R71,U7,L72'),
-    parseTrace('U62,R66,U55,R34,D71,R55,D58,R83')
-  ]), 159)
-)
+console.log(equals(nearestCrossoverToZero(['R8,U5,L5,D3','U7,R6,D4,L4']), 6))
+console.log(equals(nearestCrossoverToZero(['R75,D30,R83,U83,L12,D49,R71,U7,L72','U62,R66,U55,R34,D71,R55,D58,R83']), 159))
+console.log(equals(nearestCrossoverToZero(['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51','U98,R91,D20,R16,D67,R40,U7,R15,U6,R7']), 135))
 
 module.exports = {a, b}
 

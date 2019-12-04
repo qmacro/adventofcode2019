8c460aa6bb5216be6590156c87cb319e98b4428b
DJ Adams <dj.adams@pobox.com>
2019-12-04 11:03:52 +0000

    mods for 4b

--- day/04.js
+++ day/04.js
@@ -3,16 +3,23 @@ const { range, allPass, compose, groupWith, filter, isEmpty, complement, gt, __,
 
 const neverDecrease = n => all(identity, map(apply(lte), zip(n,tail(n))))
 
-const longerThanOne = compose(gt(__, 1), length)
-const hasSameAdjacentDigits = pipe(
+const moreThanOne = compose(gt(__, 1), length)
+const exactlyTwo = compose(equals(2), length)
+
+const hasSameAdjacentDigits = adjacencyCriteria => pipe(
   groupWith(equals),
-  filter(longerThanOne),
+  filter(adjacencyCriteria),
   complement(isEmpty)
 )
 
 const part1 = x => {
   const candidate = String(x)
-  return allPass([neverDecrease, hasSameAdjacentDigits])(candidate)
+  return allPass([neverDecrease, hasSameAdjacentDigits(moreThanOne)])(candidate)
+}
+
+const part2 = x => {
+  const candidate = String(x)
+  return allPass([neverDecrease, hasSameAdjacentDigits(exactlyTwo)])(candidate)
 }
 
 const testsets = [
@@ -25,15 +32,25 @@ const testsets = [
       [223450, false],
       [123789, false]
     ],
+  },
+
+  // -> Second part
+  {
+    fn: part2,
+    data: [
+      [112233, true],
+      [123444, false],
+      [111122, true]
+    ],
   }
 ]
 
 const runTest = (fn, [input, res]) => ([equals(fn(input), res) ? '✔' :  '❌', res])
-//console.log(map(testset => map(data => runTest(testset['fn'], data), testset['data']), testsets))
+console.log(map(testset => map(data => runTest(testset['fn'], data), testset['data']), testsets))
 
 
 const a = _ => compose(length, filter(identity), map(part1))(range(347312,805916))
-const b = _ => 'b'
+const b = _ => compose(length, filter(identity), map(part2))(range(347312,805916))
 
 
 module.exports = { a, b }

c92f420ed5089916e55b150307a6ef9f09903f98
DJ Adams <dj.adams@pobox.com>
2019-12-05 11:32:02 +0000

    change double quotes to single quotes aaargh

--- test/05.spec.js
+++ test/05.spec.js
@@ -1,11 +1,11 @@
 const day = require('../day/05')
 
-describe("Instruction parsing", () => {
-  it("Parses opcodes from single digit instructions", () => {
+describe('Instruction parsing', () => {
+  it('Parses opcodes from single digit instructions', () => {
     expect(day.parseInstruction('1').opcode).toEqual(1)
     expect(day.parseInstruction('2').opcode).toEqual(2)
   })
-  it("Parses opcodes from double digit instructions", () => {
+  it('Parses opcodes from double digit instructions', () => {
     expect(day.parseInstruction('99').opcode).toEqual(99)
   })
 })

5583c86d81d23c3bc9ada8fbfa7517566c2f761e
DJ Adams <dj.adams@pobox.com>
2019-12-05 11:45:31 +0000

    add more instruction parsing tests

--- test/05.spec.js
+++ test/05.spec.js
@@ -1,11 +1,28 @@
 const day = require('../day/05')
 
-describe('Instruction parsing', () => {
-  it('Parses opcodes from single digit instructions', () => {
+describe('Opcode parsing', () => {
+  it('Parses single digit opcodes', () => {
     expect(day.parseInstruction('1').opcode).toEqual(1)
     expect(day.parseInstruction('2').opcode).toEqual(2)
   })
-  it('Parses opcodes from double digit instructions', () => {
+  it('Parses double digit opcodes', () => {
     expect(day.parseInstruction('99').opcode).toEqual(99)
+  })
+  it('Parses opcodes with more than two digits', () => {
+    expect(day.parseInstruction('1002').opcode).toEqual(02)
+  })
+})
+
+describe('Parameter mode parsing', () => {
+  it('Determines the explicit mode of a single parameter', () => {
+    expect(day.parseInstruction('102').mode(1)).toEqual(1)
+    expect(day.parseInstruction('123').mode(1)).toEqual(1)
+    expect(day.parseInstruction('1023').mode(1)).toEqual(0)
+    expect(day.parseInstruction('1023').mode(2)).toEqual(1)
+  })
+  it('Determines the implicit mode of a single parameter', () => {
+    expect(day.parseInstruction('2').mode(1)).toEqual(0)
+    expect(day.parseInstruction('123').mode(2)).toEqual(0)
+    expect(day.parseInstruction('1').mode(10)).toEqual(0)
   })
 })
